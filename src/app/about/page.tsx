eeb2d47ed18817b59e28e493a09020e109be14df
rory <rorysaxton@gmail.com>
2024-01-20 18:20:12 -0700

    about stylings

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,15 @@
 "use client"
 import Navbar from "../../../components/Navbar";
 import AuthContextProvider from "../../../context/AuthContext";
+import { themes } from "../../../utils/themes";
 
 export default function Home (){
 return(
     
     <AuthContextProvider>
 
-<Navbar theme="black"/>
-    <div style={{paddingTop:"5em",color:"white",top:0,left:0,position:"fixed",backgroundColor:"black",width:"100vw",height:"100%",textAlign:"center"}}>
+<Navbar theme="red"/>
+    <div style={{paddingTop:"5em",color:themes.red.link,top:0,left:0,position:"fixed",backgroundColor:themes.red.base,width:"100vw",height:"100%",textAlign:"center"}}>
 <h1 >About myStuff:</h1>
 
 <h2>myStuff allows you to make a home page for your online accounts.</h2>
@@ -16,7 +17,7 @@ return(
     <h1>About me:</h1>
 <h2> My name is Rory Saxton and I&apos;m a self taught web-developer.</h2>
 <h2>If you have questions feel free to email me at contact@rorysaxton.com</h2>
-<h2>My other work can be found at <a style={{color:"white"}} href="https://rorysaxton.com/">
+<h2>My other work can be found at <a style={{color:themes.red.link}} href="https://rorysaxton.com/">
     rorysaxton.com
     </a></h2>
 <p>Thank you for checking out this project!</p>
